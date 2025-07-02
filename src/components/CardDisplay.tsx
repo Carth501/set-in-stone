9337fd6c69ce50b2fd007d2500932613ecff5a02
carth501 <carth501@gmail.com>
2025-07-02 15:49:41 -0400

    Change the card header

--- src/components/CardDisplay.tsx
+++ src/components/CardDisplay.tsx
@@ -16,38 +16,39 @@ const CardDisplay: React.FC<Props> = ({ card, className }) => {
   return (
     <div className="card-container">
       <div className={`card ${cardColorClass} ${className}`}>
-        <div className="card-header bg-gray-800/25 rounded px-2">
-          <div className="header-left">
+        <div className="card-header bg-gray-800/25 rounded px-2 flex flex-col">
+          <div className="flex justify-between">
             <div className="card-name max-w-330px max-h-72px wrap-break-word">
               {card.name}
             </div>
-            <div className="card-type">{card.type}</div>
+            <span className="card-costs">
+              <div
+                style={{
+                  display: "flex",
+                  flexWrap: "wrap",
+                  gap: "0.5em",
+                  justifyContent: "flex-end",
+                }}
+              >
+                {Object.entries(card.aspectList ?? {}).flatMap(
+                  ([aspect, count]) =>
+                    count > 0
+                      ? Array.from({ length: count }).map((_, i) => (
+                          <img
+                            key={aspect + "-" + i}
+                            src={
+                              AspectIcons[aspect as keyof typeof AspectIcons]
+                            }
+                            alt={aspect}
+                            style={{ width: 32, height: 32 }}
+                          />
+                        ))
+                      : []
+                )}
+              </div>
+            </span>
           </div>
-          <span className="card-costs">
-            <div
-              style={{
-                display: "flex",
-                flexWrap: "wrap",
-                gap: "0.5em",
-                maxWidth: 120,
-                justifyContent: "flex-end",
-              }}
-            >
-              {Object.entries(card.aspectList ?? {}).flatMap(
-                ([aspect, count]) =>
-                  count > 0
-                    ? Array.from({ length: count }).map((_, i) => (
-                        <img
-                          key={aspect + "-" + i}
-                          src={AspectIcons[aspect as keyof typeof AspectIcons]}
-                          alt={aspect}
-                          style={{ width: 32, height: 32 }}
-                        />
-                      ))
-                    : []
-              )}
-            </div>
-          </span>
+          <div className="card-type">{card.type}</div>
         </div>
         <div style={{ flex: 1, marginBottom: 16 }}>{card.description}</div>
         <div
