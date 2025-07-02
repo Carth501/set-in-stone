f60120d1149e13fcfa9e312e1ba56ac98ea7e03c
carth501 <carth501@gmail.com>
2025-07-02 16:40:59 -0400

    Handle multiple aspects on a card.

--- src/utils/cardColors.ts
+++ src/utils/cardColors.ts
@@ -1,14 +1,6 @@
 import type { Aspect } from "../aspects/aspects";
 import type { CardCost } from "../types/Card";
 
-const aspectColorMap: Record<Aspect, string> = {
-  BLOOM: "bloom",
-  CALLOUS: "callous",
-  LAW: "law",
-  TIDE: "tide",
-  ZEAL: "zeal",
-};
-
 export function getCardColorClass(aspectList: CardCost): string {
   const aspectsWithCost = Object.entries(aspectList)
     .filter(([, count]) => count > 0)
@@ -24,11 +16,10 @@ export function getCardColorClass(aspectList: CardCost): string {
 
   if (aspectsWithCost.length === 2) {
     const [first, second] = aspectsWithCost;
-    return `bg-gradient-to-br from-${aspectColorMap[first]} to-${aspectColorMap[second]}`;
+    return getDoubleAspectColor(first, second);
   }
 
-  const [first, second, third] = aspectsWithCost;
-  return `bg-gradient-to-br from-${aspectColorMap[first]} via-${aspectColorMap[second]} to-${aspectColorMap[third]}`;
+  return "bg-poly";
 }
 
 export function getSingleAspectColor(aspect: Aspect): string {
@@ -46,4 +37,50 @@ export function getSingleAspectColor(aspect: Aspect): string {
     default:
       return "bg-fundamental";
   }
+}
+
+export function getDoubleAspectColor(aspect1: Aspect, aspect2: Aspect): string {
+  let fromClass;
+  switch (aspect1) {
+    case "BLOOM":
+      fromClass = "from-bloom";
+      break;
+    case "CALLOUS":
+      fromClass = "from-callous";
+      break;
+    case "LAW":
+      fromClass = "from-law";
+      break;
+    case "TIDE":
+      fromClass = "from-tide";
+      break;
+    case "ZEAL":
+      fromClass = "from-zeal";
+      break;
+    default:
+      fromClass = "from-fundamental";
+      break;
+  }
+  let toClass;
+  switch (aspect2) {
+    case "BLOOM":
+      toClass = "to-bloom";
+      break;
+    case "CALLOUS":
+      toClass = "to-callous";
+      break;
+    case "LAW":
+      toClass = "to-law";
+      break;
+    case "TIDE":
+      toClass = "to-tide";
+      break;
+    case "ZEAL":
+      toClass = "to-zeal";
+      break;
+    default:
+      toClass = "to-fundamental";
+      break;
+  }
+  return `bg-linear-110  ${fromClass} from-40% ${toClass} to-60%`;
 }
