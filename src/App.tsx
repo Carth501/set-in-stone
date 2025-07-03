f9fea7d86e7cc66e00c40007f73e4f889363396b
carth501 <carth501@gmail.com>
2025-07-03 17:50:39 -0400

    Use html2canvas-pro to fix error on export

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import html2canvas from "html2canvas";
+import html2canvas from "html2canvas-pro";
 import { useState } from "react";
 import "./App.css";
 import CardEditor from "./components/CardEditor.tsx";
