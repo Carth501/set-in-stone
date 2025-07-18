8a308ee0bfaeb8f12ca08c7865c9a78ed62b02f4
carth501 <carth501@gmail.com>
2025-07-18 17:02:16 -0400

    Add Tooltip component and integrate it into AspectSymbolSelector

--- src/components/AspectSymbolSelector.tsx
+++ src/components/AspectSymbolSelector.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ASPECTS, type AspectType, Icons } from "../aspects/aspects";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
 
 type Props = {
   aspects?: AspectType[];
@@ -16,7 +17,10 @@ const AspectSymbolSelector: React.FC<Props> = ({
   aspectCounts,
   vertical = false,
 }) => (
-  <div className={`flex ${vertical ? "flex-col" : "flex-row"} gap-4`}>
+  <div
+    className={`flex ${vertical ? "flex-col" : "flex-row"} gap-4`}
+    id="aspect-symbol-selector"
+  >
     {aspects.map((aspect, idx) => {
       const count = aspectCounts?.[aspect];
       if (aspectCounts && (count === undefined || count === 0 || count < 0)) {
@@ -24,23 +28,30 @@ const AspectSymbolSelector: React.FC<Props> = ({
       }
       return (
         <div key={aspect} className="flex flex-col items-center">
-          <button
-            type="button"
-            className={`rounded-full w-12 h-12 flex items-center justify-center border-2 transition
+          <Tooltip>
+            <TooltipTrigger>
+              <button
+                type="button"
+                className={`rounded-full w-12 h-12 flex items-center justify-center border-2 transition
               ${
                 selectedIndex === idx
                   ? "border-blue-500 ring-2 ring-blue-300"
                   : "border-gray-400"
               }
               bg-gray-800 hover:bg-gray-700`}
-            onClick={() => onSelect(idx)}
-          >
-            <img
-              src={Icons[aspect]}
-              alt={aspect}
-              className="w-8 h-8 object-contain"
-            />
-          </button>
+                onClick={() => onSelect(idx)}
+              >
+                <img
+                  src={Icons[aspect]}
+                  alt={aspect}
+                  className="w-8 h-8 object-contain"
+                />
+              </button>
+            </TooltipTrigger>
+            <TooltipContent sideOffset={4} side="right">
+              {aspect}
+            </TooltipContent>
+          </Tooltip>
           {aspectCounts && count && count > 0 && (
             <span className="text-xs mt-1 text-center">{count}</span>
           )}
