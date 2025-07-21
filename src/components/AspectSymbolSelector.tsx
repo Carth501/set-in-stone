2366c66ccbcd90a8dac5a54e56427c9097e39f4b
carth501 <carth501@gmail.com>
2025-07-21 14:17:36 -0400

    Refactor AspectSymbolSelector: simplify TooltipTrigger by removing unnecessary button element

--- src/components/AspectSymbolSelector.tsx
+++ src/components/AspectSymbolSelector.tsx
@@ -29,24 +29,21 @@ const AspectSymbolSelector: React.FC<Props> = ({
       return (
         <div key={aspect} className="flex flex-col items-center">
           <Tooltip>
-            <TooltipTrigger>
-              <button
-                type="button"
-                className={`rounded-full w-12 h-12 flex items-center justify-center border-2 transition
+            <TooltipTrigger
+              onClick={() => onSelect(idx)}
+              className={`rounded-full w-12 h-12 flex items-center justify-center border-2 transition
               ${
                 selectedIndex === idx
                   ? "border-blue-500 ring-2 ring-blue-300"
                   : "border-gray-400"
               }
               bg-gray-800 hover:bg-gray-700`}
-                onClick={() => onSelect(idx)}
-              >
-                <img
-                  src={Icons[aspect]}
-                  alt={aspect}
-                  className="w-8 h-8 object-contain"
-                />
-              </button>
+            >
+              <img
+                src={Icons[aspect]}
+                alt={aspect}
+                className="w-8 h-8 object-contain"
+              />
             </TooltipTrigger>
             <TooltipContent sideOffset={4} side="right">
               {aspect}
