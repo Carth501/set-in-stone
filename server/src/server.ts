c7a5c48099da628049bac488e1a04fb560d1385e
carth501 <carth501@gmail.com>
2025-08-07 13:04:38 -0400

    Redirect server to the client

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,20 +4,17 @@ import express, { Request, Response } from "express";
 const app = express();
 const PORT: number = parseInt(process.env.PORT || "5000");
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "Set in Stone API Server is running!" });
+  res.redirect("http://localhost:5173/");
 });
 
 app.get("/api/test", (req: Request, res: Response) => {
   res.json({ message: "API is working correctly" });
 });
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
